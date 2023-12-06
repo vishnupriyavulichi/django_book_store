82ba2f9fd5a90a7d4f29ad8cf01d428215336c09
vishnupriyavulichi <AasrithaSaiManyam@my.unt.edu>
2023-12-05 18:11:24 -0600

    minor change

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "staticfiles")
 MEDIA_URL = '/media/'
 
 #MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "files/")
